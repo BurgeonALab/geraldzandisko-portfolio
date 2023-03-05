db48e790d5a216c9ed80e6e50b00937e6e3e9290
gerraour <imgerraour@gmail.com>
2023-03-05 18:27:47 +0700

    social link add, responsive fix, work section add

---
+++ public/src/js/scripts.js
@@ -0,0 +1,11 @@
+const tnslider = tns({
+    "container": ".my-slider",
+    "items": 1,
+    "mouseDrag": true,
+    "controls": false,
+    "nav": false,
+    "speed": 500,
+    "gutter": 50,
+    "edgePadding": 100,
+    "autoplayHoverPause": "false"
+})
