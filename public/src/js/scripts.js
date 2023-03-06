c6adebda7732fb8ba1e5f493215e964c5a66ead5
gerraour <imgerraour@gmail.com>
2023-03-07 05:25:51 +0700

    notable work and layout alter

--- public/src/js/scripts.js
+++ public/src/js/scripts.js
@@ -1,11 +1,24 @@
-const tnslider = tns({
+const tnslider1 = tns({
     "container": ".my-slider",
     "items": 1,
     "mouseDrag": true,
     "controls": false,
     "nav": false,
     "speed": 500,
-    "gutter": 50,
+    "gutter": 20,
     "edgePadding": 100,
-    "autoplayHoverPause": "false"
+    "autoplayHoverPause": "false",
+    "preventScrollOnTouch": "auto"
+})
+
+const tnslider2 = tns({
+    "container": ".work-list-slider",
+    "mouseDrag": true,
+    "controls": false,
+    "nav": false,
+    "speed": 500,
+    "autoWidth": true,
+    "gutter": 20,
+    "autoplayHoverPause": "false",
+    "preventScrollOnTouch": "auto"
 })
