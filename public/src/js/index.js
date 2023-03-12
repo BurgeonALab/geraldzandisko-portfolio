2b8cd5fe92b429615fac4583e6c4371001c0c9ba
gerraour <imgerraour@gmail.com>
2023-03-12 15:58:38 +0700

    firebase settings

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,4 +1,7 @@
 import { initializeApp } from "firebase/app"
+import { getStorage } from "firebase/storage";
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
+
 const firebaseConfig = {
     apiKey: "AIzaSyCU5SmyCTAmXUAUVeSCf0fVnA2OxlXHU-U",
     authDomain: "gerraour-bee69.firebaseapp.com",
@@ -9,4 +12,12 @@ const firebaseConfig = {
     appId: "1:923996121424:web:1bc2f96ba78ec9a7062455",
     measurementId: "G-XDPT9XMJXM"
   };
-  const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6LdXM_QkAAAAAOacKvXRl721QlDApYHOstWXKjFV'),
+  
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true
+});
+const storage = getStorage(app);
