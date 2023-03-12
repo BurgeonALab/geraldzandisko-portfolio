95c1628bda7c9b5ecdd9dc59257b48d13de76080
gerraour <imgerraour@gmail.com>
2023-03-12 12:51:24 +0700

    webpack init

---
+++ public/src/js/index.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCU5SmyCTAmXUAUVeSCf0fVnA2OxlXHU-U",
+    authDomain: "gerraour-bee69.firebaseapp.com",
+    databaseURL: "https://gerraour-bee69-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "gerraour-bee69",
+    storageBucket: "gerraour-bee69.appspot.com",
+    messagingSenderId: "923996121424",
+    appId: "1:923996121424:web:1bc2f96ba78ec9a7062455",
+    measurementId: "G-XDPT9XMJXM"
+  };
+
+  const app = initializeApp(firebaseConfig);
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    entry: './public/src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, './public/dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'eval-source-map',
+};
