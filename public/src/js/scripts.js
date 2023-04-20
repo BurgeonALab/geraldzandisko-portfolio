e26bedb6300b583c56443ddd2aa881c4fdfcf18d
gerraour <imgerraour@gmail.com>
2023-04-20 18:34:30 +0700

    incomplete dark mode

--- public/src/js/scripts.js
+++ public/src/js/scripts.js
@@ -30,3 +30,46 @@ $(function() {
         $(this).parent().find('.notable-description-width').fadeToggle();
     });
 });
+
+$(document).ready(function() {
+    $('#lightdark-mode').click(function() {
+        var element = document.body;
+        element.classList.toggle("dark-mode");
+
+        var wisdom = document.getElementById("wisdomData");
+        wisdom.classList.toggle("text-dark-mode");
+
+        var work = document.getElementById("work_section");
+        work.classList.toggle("text-dark-mode");
+
+        var notable = document.getElementById("notable_section");
+        notable.classList.toggle("text-dark-mode");
+
+        var contact = document.getElementById("contact_section");
+        contact.classList.toggle("text-dark-mode");
+
+        var navigation = document.getElementById("navigation-bar");
+        navigation.classList.toggle("navigation-dark-mode");
+
+        var navbehance = document.getElementById("navbehance");
+        navbehance.classList.toggle("text-dark-mode");
+
+        var navlinkedin = document.getElementById("navlinkedin");
+        navlinkedin.classList.toggle("text-dark-mode");
+
+        var navgithub = document.getElementById("navgithub");
+        navgithub.classList.toggle("text-dark-mode");
+
+        var navinstagram = document.getElementById("navinstagram");
+        navinstagram.classList.toggle("text-dark-mode");
+
+        var aboutnav = document.getElementById("about-nav");
+        aboutnav.classList.toggle("text-dark-mode");
+
+        var worknav = document.getElementById("work-nav");
+        worknav.classList.toggle("text-dark-mode");
+
+        var contactnav = document.getElementById("contact-nav");
+        contactnav.classList.toggle("text-dark-mode");
+    });
+});
